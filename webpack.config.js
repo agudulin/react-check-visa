e3208ce426d765b6934a9fa676f523aa1bd05b4c
Alexander Gudulin <alexandr.gudulin@gmail.com>
2015-11-03 19:57:02 +0300

    production webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,32 @@
 var path = require('path');
 var autoprefixer = require('autoprefixer');
 var webpack = require('webpack');
+var webpackClean = require('clean-webpack-plugin');
+var webpackMerge = require('webpack-merge');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var nested = require('postcss-nested');
 
+var TARGET = process.env.npm_lifecycle_event;
 var ROOT_PATH = path.resolve(__dirname);
 var APP_PATH = path.resolve(ROOT_PATH, 'app');
 var BUILD_PATH = path.resolve(ROOT_PATH, 'dist');
 
-module.exports = {
-  entry: [
-    'webpack-hot-middleware/client',
-    APP_PATH
-  ],
-  devtool: 'eval-source-map',
+var VENDOR_LIBS = [
+  'classnames',
+  'isomorphic-fetch',
+  'react',
+  'react-dom',
+  'react-redux',
+  'redux',
+  'redux-thunk'
+];
+
+var common = {
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
-  output: {
-    path: BUILD_PATH,
-    publicPath: '/dist/',
-    filename: 'bundle.js'
-  },
   module: {
     loaders: [
-      {
-        test: /\.css$/,
-        loaders: ['style', 'css', 'postcss'],
-        include: APP_PATH
-      },
       {
         test: /\.jsx?$/,
         loaders: ['babel'],
@@ -40,10 +39,71 @@ module.exports = {
       nested(),
       autoprefixer({ browsers: ['ie >= 8', 'last 2 versions', '> 2%'] })
     ];
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ]
+  }
 };
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = webpackMerge(common, {
+    entry: [
+      'webpack-hot-middleware/client',
+      APP_PATH
+    ],
+    output: {
+      path: BUILD_PATH,
+      publicPath: '/dist/',
+      filename: 'bundle.js'
+    },
+    devtool: 'eval-source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css', 'postcss'],
+          include: APP_PATH
+        }
+      ]
+    },
+    plugins: [
+      new webpack.optimize.OccurenceOrderPlugin(),
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NoErrorsPlugin()
+    ]
+  });
+}
+else if (TARGET === 'build') {
+  module.exports = webpackMerge(common, {
+    entry: {
+      app: APP_PATH,
+      vendor: VENDOR_LIBS
+    },
+    output: {
+      path: BUILD_PATH,
+      filename: '[name].[chunkhash].js'
+    },
+    devtool: 'source-map',
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+          include: APP_PATH
+        }
+      ]
+    },
+    plugins: [
+      new webpackClean([BUILD_PATH]),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin(
+        'vendor',
+        '[name].[chunkhash].js'
+      ),
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify('production') }
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: { warnings: false }
+      })
+    ]
+  });
+}
+
